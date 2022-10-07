862fd8b97e638a666835fdb11316c051a3ba6b4b
Dirk Schumacher <mail.disch@gmail.com>
2022-10-07 11:16:43 +0200

    Refactored

--- get-dataset.py
+++ get-dataset.py
@@ -12,14 +12,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# KL_Tageswerte_Beschreibung_Stationen.txt contains information about all available stations
-df = pd.read_fwf('./data/KL_Tageswerte_Beschreibung_Stationen.txt', encoding='ISO-8859–1',
-                 skiprows=2,
-                 names=['Stations_id', 'von_datum', 'bis_datum',
-                        'Stationshoehe', 'geoBreite', 'geoLaenge',
-                        'Stationsname', 'Bundesland'])
-
-
 def rad2deg(radians):
     """Converts radians to degree
 
@@ -54,7 +46,7 @@ def get_distance_between_points(latitude1, longitude1, latitude2, longitude2, un
         longitude2 -- longitude of point 2
 
     Keyword Arguments:
-        unit -- the unit 'miles' or 'km' of the result (default: {'miles'})
+        unit -- the unit 'miles' or 'km' of the result (default: {'km'})
 
     Returns:
         the distance between point 1 and point 2 in miles or km
@@ -62,11 +54,11 @@ def get_distance_between_points(latitude1, longitude1, latitude2, longitude2, un
     theta = longitude1 - longitude2
 
     distance = 60 * 1.1515 * rad2deg(
-        np.arccos(
-            (np.sin(deg2rad(latitude1)) * np.sin(deg2rad(latitude2))) +
-            (np.cos(deg2rad(latitude1)) * np.cos(deg2rad(latitude2)) * np.cos(deg2rad(theta)))
-        )
-    )
+                                        np.arccos(
+                                                    (np.sin(deg2rad(latitude1)) * np.sin(deg2rad(latitude2))) +
+                                                    (np.cos(deg2rad(latitude1)) * np.cos(deg2rad(latitude2)) * np.cos(deg2rad(theta)))
+                                                )
+                                    )
 
     if unit == 'miles':
         return round(distance, 2)
@@ -93,55 +85,99 @@ def get_url_paths(url, ext=''):
         return response.raise_for_status()
     soup = BeautifulSoup(response_text, 'html.parser')
     parent = [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
     return parent
 
 
-# Compute the crossproduct of the stations with themself
-left = df[['Stations_id', 'Stationsname', 'geoBreite', 'geoLaenge']]
-right = df[['Stations_id', 'Stationsname', 'geoBreite', 'geoLaenge']]
-cart = left.merge(right, how='cross')
+def get_stations_ids(station, radius, unit):
+    """KL_Tageswerte_Beschreibung_Stationen.txt contains information about all available stations.
+       This function computes the IDs of all stations within a distance of radius.
 
-# Compute the distance between all stations
-cart['Distance'] = get_distance_between_points(cart['geoBreite_x'], cart['geoLaenge_x'],
-                                               cart['geoBreite_y'], cart['geoLaenge_y'],
-                                               unit='kilometers')
+    Arguments:
+        station -- Name of the station
+        radius -- Radius of the area around station to look for other stations
+        unit -- 'km' or 'miles'
 
-# Get the ids of the stations within 25 miles around Berlin-Tegel
-ids = cart[(cart['Stationsname_x'] == 'Berlin-Tegel') & (cart['Distance'] <= 25)][['Stations_id_y']]
-ids['Stations_id_y'] = ids['Stations_id_y'].astype(str).str.zfill(5)
+    Returns:
+        IDs of the found stations
+    """
 
-# See what files are available
-URL = 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/'
-EXT = 'zip'
-result = get_url_paths(URL, EXT)
-found_ids = [x.split('_')[4] for x in result]
-files = pd.DataFrame([result, found_ids]).T
-files.columns=['filename', 'id']
+    # KL_Tageswerte_Beschreibung_Stationen.txt contains information about all available stations
+    available_stations = pd.read_fwf(
+                                        './KL_Tageswerte_Beschreibung_Stationen.txt',
+                                        encoding='ISO-8859–1',
+                                        skiprows=2,
+                                        names=['Stations_id', 'von_datum', 'bis_datum',
+                                                'Stationshoehe', 'geoBreite', 'geoLaenge',
+                                                'Stationsname', 'Bundesland'
+                                                ]
+                                    )
 
-# The files of stations within the selected range are to be downloaded
-files['Download'] = files['id'].isin(ids['Stations_id_y'])
-files['localname'] = files.filename.str[97:]
-to_be_downloaded = files.loc[files['Download'] == True]
+    # Compute the crossproduct of the stations with themself
+    left = available_stations[['Stations_id', 'Stationsname', 'geoBreite', 'geoLaenge']]
+    right = available_stations[['Stations_id', 'Stationsname', 'geoBreite', 'geoLaenge']]
+    cart = left.merge(right, how='cross')
 
-# Download the files
-for row in to_be_downloaded.itertuples(index=False):
-    URL = row.filename
-    res = requests.get(URL)
-    open('./data/' + row.localname, "wb").write(res.content)
+    # Compute the distance between all stations
+    cart['Distance'] = get_distance_between_points(
+                                                    cart['geoBreite_x'], cart['geoLaenge_x'],
+                                                    cart['geoBreite_y'], cart['geoLaenge_y'],
+                                                    unit=unit
+                                                  )
+
+    # Get the ids of the stations within 25 miles around Berlin-Tegel
+    ids = cart[(cart['Stationsname_x'] == station) & (cart['Distance'] <= radius)][['Stations_id_y']]
+    ids['Stations_id_y'] = ids['Stations_id_y'].astype(str).str.zfill(5)
+
+    return ids
 
 
-# Unzip all zipfiles in the data directory
-PATH = './data'
-EXT = ".zip"
+def get_dwd_data (station, radius, unit):
+    """Get the DWD weather data of all stations within a distance of radius around station
 
-for item in os.listdir(PATH):
-    if item.endswith(EXT):
-        file_name = PATH + '/' + item
-        with zipfile.ZipFile(file_name) as zip_ref:
-            zip_ref.extractall(PATH)
+    Arguments:
+        station -- Name of the station
+        radius -- Radius of the area around station to look for other stations
+        unit -- 'km' or 'miles'
+    """
+    # See what files are available on the DWD website
+    URL = 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/'
+    EXT = 'zip'
+    result = get_url_paths(URL, EXT)
+    found_ids = [x.split('_')[4] for x in result]
+    files = pd.DataFrame([result, found_ids]).T
+    files.columns=['filename', 'id']
 
-# Cleanup: delete all files except the zip files and the files for the dataset
-for item in os.listdir(PATH):
-    if ( not item.startswith('produkt')) and (not item.startswith('tageswerte')):
-        file_name = PATH + '/' + item
-        os.remove(file_name)
+    # Get the IDs of the stations within a radius distance around station
+    ids = get_stations_ids(station, radius, unit)
+
+    # The files of stations within the selected range are to be downloaded
+    files['Download'] = files['id'].isin(ids['Stations_id_y'])
+    files['localname'] = files.filename.str[97:]
+    to_be_downloaded = files.loc[bool(files['Download'])]
+
+    # Download the files
+    for row in to_be_downloaded.itertuples(index=False):
+        url = row.filename
+        res = requests.get(url)
+        open('./data/' + row.localname, "wb").write(res.content)
+
+    # Unzip all zipfiles in the data directory
+    PATH = './data'
+    EXT = ".zip"
+    for item in os.listdir(PATH):
+        if item.endswith(EXT):
+            file_name = PATH + '/' + item
+            with zipfile.ZipFile(file_name) as zip_ref:
+                zip_ref.extractall(PATH)
+
+    # Cleanup: delete all files except files of the dataset
+    for item in os.listdir(PATH):
+        if (not item.startswith('produkt')):
+            file_name = PATH + '/' + item
+            os.remove(file_name)
+
+
+if __name__ == "__main__":
+    # Get the data of station within a distance of 25 km around Berlin-Tegel
+    get_dwd_data('Berlin-Tegel', 25, 'km')
